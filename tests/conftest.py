7806641196d8e46675a0f30eacccd95122afa89f
JnyJny <erik.oshaughnessy@gmail.com>
2020-07-09 12:08:42 -0500

    Updated test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,26 +7,6 @@ from bitvector import BitVector
 
 
 @pytest.fixture
-def zeros() -> int:
-    return 0x0000_0000_0000_0000_0000_0000_0000_0000
-
-
-@pytest.fixture
-def ones() -> int:
-    return 0xFFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF_FFFF
-
-
-@pytest.fixture
-def alphas() -> int:
-    return 0xAAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA
-
-
-@pytest.fixture
-def fives() -> int:
-    return 0x5555_5555_5555_5555_5555_5555_5555_5555
-
-
-@pytest.fixture
 def BV_0() -> BitVector:
     return BitVector(0)
 
@@ -34,3 +14,8 @@ def BV_0() -> BitVector:
 @pytest.fixture
 def BV_1() -> BitVector:
     return BitVector(1)
+
+
+@pytest.fixture
+def BV_HI() -> BitVector:
+    return BitVector(0x8000_0000_0000_0000_0000_0000_0000_0000)
