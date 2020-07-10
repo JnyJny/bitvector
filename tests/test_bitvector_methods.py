c008d23ae2803e1f35168177622d5c6d54f3bdb6
JnyJny <erik.oshaughnessy@gmail.com>
2020-07-10 14:59:24 -0500

    Initial commit of methods tests

---
+++ tests/test_bitvector_methods.py
@@ -0,0 +1,32 @@
+"""
+"""
+
+import pytest
+
+from bitvector import BitVector
+
+
+def test_bitvector_method_set(BV_0: BitVector):
+
+    assert BV_0 == 0
+    BV_0.set()
+    assert all(BV_0)
+
+
+def test_bitvector_method_clear(BV_SET: BitVector):
+
+    assert all(BV_SET)
+    BV_SET.clear()
+    assert not any(BV_SET)
+
+
+@pytest.mark.parametrize("offset", list(range(0, 128)))
+def test_bitvector_method_toggle(offset: int, BV_0: BitVector):
+
+    assert BV_0[offset] == 0
+
+    BV_0.toggle(offset)
+    assert BV_0[offset] == 1
+
+    BV_0.toggle(offset)
+    assert BV_0[offset] == 0
